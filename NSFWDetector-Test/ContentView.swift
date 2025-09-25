f509e4aae0a7089563cef8d4f49cdc5532dd3cfd
Mehmet TIRPAN <mehmettirpan92@gmail.com>
2025-09-25 23:08:44 +0300

    Initial Commit

---
+++ NSFWDetector-Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NSFWDetector-Test
+//
+//  Created by Mehmet Tırpan on 25.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NSFWDetector-Test/NSFWDetector_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSFWDetector_TestApp.swift
+//  NSFWDetector-Test
+//
+//  Created by Mehmet Tırpan on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct NSFWDetector_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
